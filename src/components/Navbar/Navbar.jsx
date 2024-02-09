4b058a0e0dca633f479ff37654384fe3280e9074
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2024-02-09 18:02:57 +0600

    added dark mode features

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import Logo from '../../assets/todo.png';
+import Moon from "../../assets/moon.svg";
+import Sun from "../../assets/sun.svg";
 
 const Navbar = () => {
+
+    const { darkMode, setDarkMode } = useState(false);
+
     return (
         <nav className="py-4 px-2 md:py-6 w-full shadow">
             <div className="max-w-7xl mx-auto flex items-center justify-between gap-x-6">
@@ -13,8 +19,16 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div>
-                    <img className='w-10 h-10 rounded-full object-cover' src="https://i.ibb.co/1qJsYCh/avatar.jpg" alt="avatar" />
+                <div className="flex items-center space-x-5">
+                    <div
+                        className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                        href="#"
+                        onClick={() => setDarkMode(darkMode => !darkMode)}
+                    >
+                        <img className='w-8 h-8 rounded-full object-cover' src={darkMode ? Sun : Moon} alt="theme-icon" />
+                    </div>
+
+                    <img className='w-8 h-8 rounded-full object-cover' src="https://i.ibb.co/1qJsYCh/avatar.jpg" alt="avatar" />
                 </div>
             </div>
         </nav>
